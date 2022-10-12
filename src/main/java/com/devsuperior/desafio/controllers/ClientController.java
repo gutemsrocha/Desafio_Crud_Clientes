1d03116be6ef54d249f3ef825c68e93c97e9646b
Gutembergue Junior <gutemsrocha@gmail.com>
2022-10-12 16:19:53 -0300

    GET Clients paged

--- src/main/java/com/devsuperior/desafio/controllers/ClientController.java
+++ src/main/java/com/devsuperior/desafio/controllers/ClientController.java
@@ -1,15 +1,17 @@
 package com.devsuperior.desafio.controllers;
 
 import com.devsuperior.desafio.dto.ClientDTO;
-import com.devsuperior.desafio.entities.Client;
-import com.devsuperior.desafio.repositories.ClientRepository;
 import com.devsuperior.desafio.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -22,6 +24,11 @@ public class ClientController {
     @GetMapping(value = "/{id}")
     public ClientDTO findById(@PathVariable Long id) {
         return service.findById(id);
+    }
+
+    @GetMapping
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        return service.findAll(pageable);
     }
 
 
--- src/main/java/com/devsuperior/desafio/services/ClientService.java
+++ src/main/java/com/devsuperior/desafio/services/ClientService.java
@@ -4,9 +4,12 @@ import com.devsuperior.desafio.dto.ClientDTO;
 import com.devsuperior.desafio.entities.Client;
 import com.devsuperior.desafio.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -20,6 +23,13 @@ public class ClientService {
         Client client = repository.findById(id).get();
         return new ClientDTO(client);
     }
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        Page<Client> result = repository.findAll(pageable);
+        return result.map(x -> new ClientDTO(x));
+    }
+
 
 
 }
